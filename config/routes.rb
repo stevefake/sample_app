ed3c1974777efba3838d0afc52db4d8cc26654d0
Steve Fake <steven.m.fake@gmail.com>
2016-06-29 14:47:46 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 
 end
